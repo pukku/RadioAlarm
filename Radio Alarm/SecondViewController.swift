897d564dbe34816f74994f2c8ac3ed96f972762e
Ricky Morse <pukku@mac.com>
2015-09-26 15:38:15 -0400

    Initial Commit

---
+++ Radio Alarm/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Radio Alarm
+//
+//  Created by Ricky Morse on 9/26/15.
+//  Copyright © 2015 Ricky Morse. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
